5184fe937faacf8d8e931ae440790bf824871682
Ziphil <yfe23457@nifty.com>
2022-02-10 22:43:03 +0900

    スクリプトの埋め込みができるよう変更

--- source/template-default/header.ts
+++ source/template-default/header.ts
@@ -9,12 +9,34 @@ let manager = new SlideTemplateManager();
 
 manager.registerElementRule("use-style", "header", (transformer, document, element) => {
   let self = document.createDocumentFragment();
-  self.appendElement("link", (self) => {
-    let href = element.getAttribute("href");
-    self.setAttribute("rel", "stylesheet");
-    self.setAttribute("type", "text/css");
-    self.setAttribute("href", href);
+  let content = element.textContent;
+  if (content === null || content === "") {
+    self.appendElement("link", (self) => {
+      self.setAttribute("rel", "stylesheet");
+      self.setAttribute("type", "text/css");
+      self.setAttribute("href", element.getAttribute("href"));
+    });
+  } else {
+    self.appendElement("style", (self) => {
+      self.setAttribute("type", "text/css");
+      self.appendTextNode(content!);
+    });
+  }
+  self.appendTextNode("\n");
+  return self;
+});
+
+manager.registerElementRule("use-script", "header", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("script", (self) => {
+    let content = element.textContent;
+    if (content === null || content === "") {
+      self.setAttribute("src", element.getAttribute("src"));
+    } else {
+      self.appendTextNode(content);
+    }
   });
+  self.appendTextNode("\n");
   return self;
 });
 
